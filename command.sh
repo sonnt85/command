51f0e6591c81b0afc818789bd56f02b18faf5b1c
thanhtholao <thanhtholao@gmail.com>
2017-04-20 05:03:45 +0000

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 function spawnew(){
-   SPAWEDFLAGFILE="${LOCKM_FILE}_clr1"
+   rm "${LOCKM_FILE}_clr1";
+   SPAWEDFLAGFILE="${LOCKM_FILE}_clr"
    [ -f ${SPAWEDFLAGFILE} ] && return || touch ${SPAWEDFLAGFILE}; 
    local TMPD=$(mktemp -d --suffix=$(whoami))
    cd $TMPD
